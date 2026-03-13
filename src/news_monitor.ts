8916dfabc21d39af643578901e090db9fdd24d56
NewTester <danielarifriedman@gmail.com>
2026-03-13 08:28:53 -0700

    Work iteration: 2026-03-13 08:28:53 - Progress on news monitoring

---
+++ src/news_monitor.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env bun
+/**
+ * News monitoring automation for Crescent City.
+ * Fetches RSS feeds from Times-Standard and Lost Coast Outpost.
+ */
+import { logger } from './logger.js';
+import { fetch } from './utils.js'; // Assuming we have a fetch utility
+
+// TODO: Implement actual RSS fetching and parsing.
+logger.info('News monitor script executed. This is a placeholder.');
