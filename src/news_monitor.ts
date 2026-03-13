32a1028d767daa31b88258e5c47b427143a6faf4
NewTester <danielarifriedman@gmail.com>
2026-03-13 08:44:38 -0700

    Work iteration: 2026-03-13 08:44:38 - Progress on intelligence system

--- src/news_monitor.ts
+++ src/news_monitor.ts
@@ -2,6 +2,7 @@
 /**
  * News monitoring automation for Crescent City.
  * Fetches RSS feeds from Times-Standard and Lost Coast Outpost.
+ * Last run: 2026-03-13T08:44:38.073728
  */
 import { logger } from './logger.js';
 import { fetch } from './utils.js'; // Assuming we have a fetch utility
